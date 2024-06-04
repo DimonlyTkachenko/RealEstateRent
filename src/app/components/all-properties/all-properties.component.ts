11c80ee7d523a0f8d0c0230b52b52bef337951fe
Dmytro Tkachenko <ia03.tkachenko.dmytro@gmail.com>
2024-06-05 00:15:45 +0300

    added logic to all properties filters and updated ui

--- src/app/components/all-properties/all-properties.component.ts
+++ src/app/components/all-properties/all-properties.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RealEstateService } from '../../services/real-estate.service';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
@@ -18,21 +19,62 @@ import { MatRadioModule } from '@angular/material/radio';
     MatInputModule,
     MatButtonModule,
     MatRadioModule,
-
+    ReactiveFormsModule,
     RouterLink,
   ],
   templateUrl: './all-properties.component.html',
-  styleUrl: './all-properties.component.scss',
+  styleUrls: ['./all-properties.component.scss'],
 })
-export class AllPropertiesComponent {
-  properties: any[];
+export class AllPropertiesComponent implements OnInit {
+  properties: any[] = [];
+  filteredProperties: any[] = [];
+  filterForm: FormGroup;
 
-  constructor(private realEstateService: RealEstateService) {}
+  constructor(private realEstateService: RealEstateService, private fb: FormBuilder) {
+    this.filterForm = this.fb.group({
+      location: [''],
+      type: [''],
+      minBudget: [''],
+      maxBudget: [''],
+      rentOrder: [''],
+    });
+  }
 
   ngOnInit(): void {
     this.realEstateService.getAllProperties().subscribe((data) => {
-      console.log(data);
       this.properties = data;
+      this.filteredProperties = data;
     });
+
+    this.filterForm.valueChanges.subscribe(() => {
+      this.applyFilters();
+    });
+  }
+
+  applyFilters(): void {
+    const { location, type, minBudget, maxBudget, rentOrder } = this.filterForm.value;
+    this.filteredProperties = this.properties.filter((property) => {
+      return (
+        (location ? property.location.toLowerCase().includes(location.toLowerCase()) : true) &&
+        (type && property.type ? property.type.toLowerCase().includes(type.toLowerCase()) : true) &&
+        (minBudget ? property.price >= minBudget : true) &&
+        (maxBudget ? property.price <= maxBudget : true)
+      );
+    });
+
+    if (rentOrder) {
+      this.filteredProperties.sort((a, b) => {
+        if (rentOrder === 'low-high') {
+          return a.price - b.price;
+        } else if (rentOrder === 'high-low') {
+          return b.price - a.price;
+        }
+        return 0;
+      });
+    }
+  }
+
+  clearFilters(): void {
+    this.filterForm.reset();
   }
 }
